cf1085386428581ab59af144054ddcdf7a43b40a
Fabrizio-xvz <adrian.fabrizio3@gmail.com>
2026-04-29 15:22:31 -0500

    logica y vista de la rama reportes de movimientos

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,8 @@
 import { Routes } from '@angular/router';
+import { Reports } from './features/reports/reports';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: 'reportes', component: Reports },
+  { path: '', redirectTo: 'reportes', pathMatch: 'full' },
+  { path: '**', redirectTo: 'reportes' },
+];
