2a70dfc8c8428600a825cd052e5f64695301bcc9
Espiritu16 <u23214543@utp.edu.pe>
2026-04-29 14:17:27 -0500

    diseno ui de sidebar

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,14 +1,7 @@
 import { Routes } from '@angular/router';
-import { Login } from './features/login/login';
 import { AppShell } from './features/layout/app-shell/app-shell';
 
 export const routes: Routes = [
-  { path: 'login', component: Login },
-  {
-    path: 'app',
-    component: AppShell,
-    children: [{ path: '', component: Login }],
-  },
-  { path: '', redirectTo: 'login', pathMatch: 'full' },
-  { path: '**', redirectTo: 'login' },
+  { path: '', component: AppShell },
+  { path: '**', redirectTo: '' },
 ];
