ce183a17db3e517e14d9ca9e0be92aa55fd8c4aa
Espiritu16 <u23214543@utp.edu.pe>
2026-04-29 14:07:10 -0500

    estructura para sidebar

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,14 @@
 import { Routes } from '@angular/router';
+import { Login } from './features/login/login';
+import { AppShell } from './features/layout/app-shell/app-shell';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: 'login', component: Login },
+  {
+    path: 'app',
+    component: AppShell,
+    children: [{ path: '', component: Login }],
+  },
+  { path: '', redirectTo: 'login', pathMatch: 'full' },
+  { path: '**', redirectTo: 'login' },
+];
