653c6847c776b7ecb50f94c1c738ce6ae3915af4
Fabrizio-xvz <adrian.fabrizio3@gmail.com>
2026-04-29 15:18:19 -0500

    vista y logica en typescript para usuarios

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,8 @@
 import { Routes } from '@angular/router';
+import { Users } from './features/users/users';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: 'usuarios', component: Users },
+  { path: '', redirectTo: 'usuarios', pathMatch: 'full' },
+  { path: '**', redirectTo: 'usuarios' },
+];
