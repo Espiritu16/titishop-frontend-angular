036045557124239e59050620713bb781ac47e83d
Fabrizio-xvz <adrian.fabrizio3@gmail.com>
2026-04-29 14:59:24 -0500

    enrutamiento para la vista de configuracion

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,8 @@
 import { Routes } from '@angular/router';
+import { Settings } from './features/settings/settings';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: 'configuracion', component: Settings },
+  { path: '', redirectTo: 'configuracion', pathMatch: 'full' },
+  { path: '**', redirectTo: 'configuracion' },
+];
