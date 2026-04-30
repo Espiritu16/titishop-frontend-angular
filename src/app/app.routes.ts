80cc7467b272c46e526918d9844a0b2fb30ce891
Fabrizio-xvz <adrian.fabrizio3@gmail.com>
2026-04-29 20:55:58 -0500

    vista y logica del negocio para la rama productos

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,8 @@
 import { Routes } from '@angular/router';
+import { Products } from './features/products/products';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: 'productos', component: Products },
+  { path: '', redirectTo: 'productos', pathMatch: 'full' },
+  { path: '**', redirectTo: 'productos' },
+];
