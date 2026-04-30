3c7cec09925bdd2a0e82942b7790bddd01eaead4
Espiritu16 <u23214543@utp.edu.pe>
2026-04-30 09:27:27 -0500

    version sin merge dev

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,7 +1,9 @@
 import { Routes } from '@angular/router';
 import { AppShell } from './features/layout/app-shell/app-shell';
+import { Login } from './features/login/login';
 
 export const routes: Routes = [
+  { path: 'login', component: Login },
   { path: '', component: AppShell },
   { path: '**', redirectTo: '' },
 ];
