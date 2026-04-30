dc128c07b7ae9dc065ce84f76beb5ddab819f413
FelixTDev <salimv2305@gmail.com>
2026-04-29 21:03:40 -0500

    vista y logica lista rama inventario

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,8 @@
 import { Routes } from '@angular/router';
+import { Inventory } from './features/inventory/inventory';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: 'inventario', component: Inventory },
+  { path: '', redirectTo: 'inventario', pathMatch: 'full' },
+  { path: '**', redirectTo: 'inventario' },
+];
