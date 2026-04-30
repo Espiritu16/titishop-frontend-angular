c0252c9eaf3763a7dd0ff5c3085f2c0ec727adef
FelixTDev <salimv2305@gmail.com>
2026-04-29 21:11:25 -0500

    logica y ui lista para la rama proveedores

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,8 @@
 import { Routes } from '@angular/router';
+import { Providers } from './features/providers/providers';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: 'proveedores', component: Providers },
+  { path: '', redirectTo: 'proveedores', pathMatch: 'full' },
+  { path: '**', redirectTo: 'proveedores' },
+];
